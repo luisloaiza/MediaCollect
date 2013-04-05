316f5b00efa13688860dee9e0382212a550bb583
Luis Loaiza <luislp05@gmail.com>
2013-04-04 22:18:31 -0400

    fresh init

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ MediaCollect
 ============
 
 Process media from remote servers. Wraps imagemagick to perform actions like resize and crop to images.
+Media Collect it's a NODEjs helper to manage different versions of a image you want to download from the web.
--- mediacollect.js
+++ mediacollect.js
@@ -1,20 +1,17 @@
 /*
-
+NODEJS LIB
 LIB created using wrappingimageMagick 
 to download an image from an URL and resize it to save it
 on DISK
-params.format,
-	                dstPath: params.finalPath+params.fileName,
-	                width: params.width,
-	                height:params.height
-
-	                fileUrl,fileName,
 
 */
 
 var urllib= require('url');
-var  im = require('imagemagick');
+var im = require('imagemagick');
 var http = require('http');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var DOWNLOAD_DIR = './downloads/';
 
 exports.getContent2Resize = function (params, onSuccess, onError) {
 	
@@ -33,14 +30,14 @@ exports.getContent2Resize = function (params, onSuccess, onError) {
     		body+=data;
             
         }).on('end', function() {
-
+        	
 			im.identify({data:body}, function(err, features){
   				if (err){
 					console.log("error in imagemagick");
 					 onError(err);
 					return;
 				}
-				console.log("image size wi "+features.width);
+				
 
 				im.resize({
 	                srcData: body,
@@ -59,6 +56,8 @@ exports.getContent2Resize = function (params, onSuccess, onError) {
 					onSuccess(params.fileName);
 	            });
             //finish resize
+			
+
 
 			});
 			//finish size of image & more...
@@ -70,16 +69,22 @@ exports.getContent2Resize = function (params, onSuccess, onError) {
 			onError(e.message);
 	});
 };
+exports.getLocalImageContent = function (filePath, onSuccess) {
+
+	im.identify(filePath, function (err, features){
+  		if(features!=null || features!=undefined)
+  			features.filePath=filePath;
+		onSuccess(err, features);
+	});
+}
 
 exports.getImageContent = function (fileUrl, onSuccess, onError) {
-
+	
 	var options = {
     	host: urllib.parse(fileUrl).host,
     	port: 80,
     	path: urllib.parse(fileUrl).pathname
 	};
-
-console.log("getting the content "+options.host+ " form "+options.path);
 
 	http.get(options, function(res) {
 		var body = '';
@@ -88,16 +93,57 @@ console.log("getting the content "+options.host+ " form "+options.path);
     res.on('data', function(data) {
     		body+=data;
         }).on('end', function() {
-
-			im.identify({data:body}, function(err, features){
-  				if (err){
-					console.log("error in imagemagick");
-					onError(err);
-					return;
-				}
-				onSuccess(body, features);
-			});
+        	try{
+        		var localError=null;
+				im.identify({data:body}, function(err, features){
+	  				if (!err){
+	  					onSuccess(body, features);
+	  					return;
+	  				}
+	  				else
+	  					localError=err;
+				});
+				if(localError!=null)
+					throw new Error("magick at identify "+localError);
 			//finish size of image & more...
+			}catch(e){
+				//try donwloading the file and then getting the data
+				console.log("try downloading.. "+fileUrl.split('/').pop());
+
+				// extract the file name
+			    //var file_name = urllib.parse(fileUrl).pathname.split('/').pop();
+			    var file_name = fileUrl.split('/').pop();
+			    // compose the wget command
+			    var wget = 'wget -P ' + DOWNLOAD_DIR + ' ' + fileUrl;
+			    // excute wget using child_process' exec function
+
+			    var child = exec(wget, function(err, stdout, stderr) {
+			        if (err) {
+			        	console.log("error in wget exec "+err.toString());
+			        	onError(err);
+			        }
+			        else{
+			        	try{
+
+
+						var imdata = fs.readFileSync(DOWNLOAD_DIR+file_name, 'binary');
+			        	im.identify({data:imdata}, function(err, features){
+			  				if (err){
+								console.log("error in identify ");
+
+								onError(err);
+								return;
+							}
+							
+							onSuccess(imdata, features);
+						});	
+			        	}
+			        	catch(e){
+			        		onError(e.toString());	
+			        	}
+			        }
+			    });
+			}
         });
 
    		
@@ -111,8 +157,35 @@ console.log("getting the content "+options.host+ " form "+options.path);
 exports.getThumbsFromImageData = function (params,thumbsList, onSuccess) {
 	var thumbprops=thumbsList.shift();
 
+	if(thumbprops.crop!=null){
+		im.crop({
+		    srcData: params.imageData,
+			srcPath: params.imageSrc,
+			gravity: "Center",
+		    dstPath: params.finalPath+thumbprops.fileName,
+		    width: thumbprops.width,
+		    height: thumbprops.height,
+		    quality: 1
+		  }, function (err, stdout, stderr){
+		    	if (err) 
+				{	
+					onSuccess(err,false);
+					return;
+				}
+				if(thumbsList.length>0)
+					module.exports.getThumbsFromImageData(params,thumbsList,onSuccess);
+				else
+					onSuccess(null,true);
+		  });
+
+		return;
+	}
+
+
+
 	im.resize({
 		srcData: params.imageData,
+		srcPath: params.imageSrc,
 		format: params.format,
 		dstPath: params.finalPath+thumbprops.fileName,
 		width: thumbprops.width,
